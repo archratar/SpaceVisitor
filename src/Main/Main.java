516543b941fb2b75dab15e49a1c8686a562e8c7a
Vasil Hlukhenkyi <tripylec@gmail.com>
2017-07-21 00:21:22 +0300

    formatted code

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -28,14 +28,18 @@ public class Main {
 
             System.out.println("Your roll is " + value + " , ");
             switch (value) {
-                case 10: tolyan.buildCabin(cabin); System.out.println(cabin);
-                    break;
-                case 30: tolyan.buildEngine(engine); System.out.println(engine);
-                    break;
-                case 50: tolyan.buildStages(stages); System.out.println(stages);
-                    break;
-                case 66: tolyan.buildStages(stages).buildCabin(cabin).buildEngine(engine); System.out.println(engine + " " + cabin + " " + stages);
-                    break;
+                case 10: tolyan.buildCabin(cabin);
+                         System.out.println(cabin);
+                         break;
+                case 30: tolyan.buildEngine(engine);
+                         System.out.println(engine);
+                         break;
+                case 50: tolyan.buildStages(stages);
+                         System.out.println(stages);
+                         break;
+                case 66: tolyan.buildStages(stages).buildCabin(cabin).buildEngine(engine);
+                         System.out.println(engine + " " + cabin + " " + stages);
+                         break;
                 default:  tolyan.leaverShot(); statusOfTolyan--;
             }
 
